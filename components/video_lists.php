5b976b6d4decae1a9ece3d957f3a7a964362bf19
BiniTutor <awakisew@gmail.com>
2025-03-21 14:13:41 -0400

    modified video lists to show view counts and upload date

--- components/video_lists.php
+++ components/video_lists.php
@@ -11,6 +11,58 @@ foreach ($videos as $video) {
         $video['description'] = 'No description available';
     }
 
+    if(!$video['view_count']){
+        $views = 0;
+    } else {
+        // sepearate by comma in 3 digits
+        $views = number_format($video['view_count'], 2, '.', ',');
+    }
+
+    if($video['created_at']){
+        // calculate the date uploaded.
+        $upload_dt = new DateTime( $video['created_at'] ); // "2007-03-24"
+        $today = new DateTime();
+        // echo 'upload_dt= ' . $upload_dt->format('Y-m-d') . '<br>'; 
+        // echo 'date2= ' . $today->format('Y-m-d') . '<br>'; 
+
+        $interval = $upload_dt->diff($today);
+        // echo "difference " . $interval->y . " years, " . $interval->m." months, ".$interval->d." days "; 
+        // difference 0 years, 3 months, 19 days 
+
+        // shows the total amount of days (not divided into years, months and days like above)
+        // echo "difference " . $interval->days . " days ";
+        // difference 111 days
+        $interval_total_days = $interval->days;
+        $interval_days = $interval->d;
+        $interval_months = $interval->m;
+        $interval_years = $interval->y;
+        $crtd_dt = '';
+        if( $interval_total_days < 30 ){
+            $crtd_dt .= $interval->d . ' days ';
+        } else if( $interval_total_days < 365 ){            
+            if ( $interval->m > 0 ){
+                $crtd_dt .= $interval->m . ' months, ';
+            }
+            if ( $interval->d > 0 ){
+                $crtd_dt .= $interval->d . ' days ';
+            }
+        }
+        else {
+            if ( $interval->y > 0 ){
+                $crtd_dt .= $interval->y . ' years, ';
+            }
+            if ( $interval->m > 0 ){
+                $crtd_dt .= $interval->m . ' months, ';
+            }
+            if ( $interval->d > 0 ){
+                $crtd_dt .= $interval->d . ' days ';
+            }
+        }
+    } 
+
+
+    //     <p>'. $video['description'] .'</p> 
+
     $v_card = '
         <div class="vid-list">
             <a href="./?vid_id=' . $video['video_id'] . '"> 
@@ -20,8 +72,8 @@ foreach ($videos as $video) {
                 <img src="./uploads/profiles/profile1.jpg" alt="$video->ppurl">
                 <div class="vid-info">
                     <a href="' . $video['video_url'] . '">'.$video['title'].'</a>
-                    <p>'. $video['description'] .'</p>
-                    <p>'. $video['view_count'] .'</p>
+                    <p>BiniTutor</p>
+                    <p>'. $views .' views • ' . $crtd_dt . ' ago</p>
                 </div>
             </div>
         </div>
